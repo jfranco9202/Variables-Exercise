d516b559aeb4de5fbf24b672b4d4e21ee26a3796
jfranco9202 <jeremy.franco41@yahoo.com>
2021-12-17 10:07:08 -0600

    Variables Exercise

---
+++ Variables_Exercise 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Variables_Exercise_1
+{
+    class Program
+    {
+        private const string V = "Wifey";
+
+        static void Main(string[] args)
+        {
+            //Variables Declaration
+            int age;
+            string name;
+            bool isAlive;
+            char myLove;
+            decimal myDecimal;
+            double myCash;
+
+
+            //Intializing Variables
+            age = 33;
+            name="Jeremy";
+            isAlive = true;
+            myLove = 'M';
+            myDecimal = 11.55m;
+            myCash = 1.11;
+
+            Console.WriteLine($"{name} is {isAlive} and {age} is this much. {myLove} is here in the house and her {myDecimal} and {myCash} is this much");
+
+
+
+
+
+
+
+              
+        }
+    }
+}
